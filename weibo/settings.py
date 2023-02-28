7e4741613d469c54e779db72fa453096327c23d4
Phoebe Gao <89928178+Peihan12@users.noreply.github.com>
2023-02-28 01:50:33 -0600

    nn

--- weibo/settings.py
+++ weibo/settings.py
@@ -7,12 +7,12 @@ COOKIES_ENABLED = False
 TELNETCONSOLE_ENABLED = False
 LOG_LEVEL = 'ERROR'
 # 访问完一个页面再访问下一个时需要等待的时间，默认为10秒
-DOWNLOAD_DELAY = 10
+DOWNLOAD_DELAY = 15
 DEFAULT_REQUEST_HEADERS = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'your cookie'
+    'cookie': 'SCF=AkFGdjVpdS32lpFFdto5PgDYWNESV3dzx5xZj3G9_7FLbw-ZjWZEsd4KfSDWyXjmFgz7_BXmrnu_5Z7u3MQU-WQ.; SUB=_2A25O_o0nDeRhGeNL41sX8y3IyDyIHXVqABNvrDV6PUNbktANLVDNkW1NSNv2hW2A4gH2oVHYeZBwgh2vJm-JREoL; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWB0Sc05i5YVVlRWYBjAhho5JpX5KMhUgL.Fo-f1h.ce0eXe052dJLoI0YLxKMLBoeL1K-LxKMLBoeL1K-LxK-L1-eLBKnLxKBLBonL122LxKqL1K.L1-2LxKnL1hMLB-2LxKBLBo.LBK5t; ALF=1679985271; _T_WM=90bc25eb0b7482c81e12b46b78d5fa0c; mp_6d39625637b58c183009d13f9971fd53_mixpanel={"distinct_id": "1868b15a69698c-022f84a3a86dd8-1f525634-157188-1868b15a697f18","$device_id": "1868b15a69698c-022f84a3a86dd8-1f525634-157188-1868b15a697f18","$initial_referrer": "https://github.com/dataabc/weiboSpider/blob/master/docs/cookie.md","$initial_referring_domain": "github.com"}'
 }
 ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
@@ -24,18 +24,18 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['迪丽热巴']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['疫情', '抗疫', '新冠', '隔离']  # 或者 KEYWORD_LIST = 'keyword_list.txt'···················
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
-WEIBO_TYPE = 1
+WEIBO_TYPE = 2
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
 CONTAIN_TYPE = 0
 # 筛选微博的发布地区，精确到省或直辖市，值不应包含“省”或“市”等字，如想筛选北京市的微博请用“北京”而不是“北京市”，想要筛选安徽省的微博请用“安徽”而不是“安徽省”，可以写多个地区，
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用“全部”
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2020-03-01'
+START_DATE = '2022-11-07'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2020-03-01'
+END_DATE = '2022-11-14'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
 FURTHER_THRESHOLD = 46
